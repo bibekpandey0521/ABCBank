da25811dc9f2c4c42188e3ac09827ba89c2fd138
bibekpandey0521 <bibekpandey276@gmail.com>
2026-03-15 21:28:54 +0545

    feat(accountHolderEndPoints/accountPoints):add account and accountHolderPoints implemented

---
+++ BankUI/Endpoints/AccountHolderEndpoints.cs
@@ -0,0 +1,14 @@
+﻿namespace BankUI.Endpoints
+{
+    public static class AccountHolderEndpoints
+    {
+        public const string AddNew = "api/accountHolders/add";
+        public const string Update = "api/accountHolders/update";
+        public const string Delete = "api/accountHolders";
+        public const string GetAll = "api/accountHolders/all";
+        public static string GetById(int id)
+        {
+            return $"api/accountholders/{id}";
+        }
+    }
+}
--- readme.md
+++ readme.md
@@ -1,10 +1,9 @@
 
 
-![Image](./docs/img/Screenshot%20(302).png)
+![Image](./docs/img/Screenshot%20(30)6.png)
 
-![Image](./docs/img/Screenshot%20(303).png)
+![Image](./docs/img/Screenshot%20(307).png)
 
-![Image](./docs/img/Screenshot%20(304).png)
+![Image](./docs/img/Screenshot%20(308).png)
 
-![Image](./docs/img/Screenshot%20(305).png)
 
